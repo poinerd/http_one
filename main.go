b2efb4f1b6f2bf321a61c3f68142a59ac2a31dba
Obadofin Emmanuel <obadofinadedapo123@gmail.com>
2026-05-26 12:33:41 +0100

    implemented the dynamic routing

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import(
 	"net"
 	"fmt"
 	"bufio"
+	"strings"
 )
 
 func main(){
 
     // set up a tcp socket
+
+
 
 	listener, err := net.Listen("tcp", "localhost:8080")
 
@@ -17,6 +20,10 @@ func main(){
 	}
 
 	for{
+
+		var clientMethod string
+		var clientPath string
+
 		conn, err := listener.Accept()
 		if err!= nil{
 			fmt.Println("An error occured while setting up a connection")
@@ -24,9 +31,25 @@ func main(){
 
 		reader := bufio.NewReader(conn)
 
+		// Detrmining the memthod and path of the client
+		requestLine , err := reader.ReadString('\n')
+
+		if err != nil{
+			fmt.Println("There was an error reading the requestLine")
+		}
+
+		parts := strings.Fields(requestLine)
+
+		if len(parts) >= 2{
+			clientMethod = parts[0]
+			clientPath = parts[1]
+
+			fmt.Printf("The client just made a %s request on path %s", clientMethod, clientPath)
+		}
+
 		for{
 			line, err := reader.ReadString('\n')
-			
+		
 			if err != nil{
 				break			
 			}
@@ -35,17 +58,35 @@ func main(){
 			if line == "\r\n" || line == "\n" || line == "wrap\n"{
 				break
 			}
-			
 		}
-		body := "<html><body><h1>Hello! You built an HTTP server from scratch!</h1></body></html>"
-	
-		responseText := "HTTP/1.1 200 Ok\r\n" +
+
+		// implementing dynamic routing 
+        var body string
+		var status string
+		
+		switch clientPath{
+		case "/" : 
+			status = "HTTP/1.1 200 Ok\r\n"
+			body = "<html><body><h1>Hello! You built an HTTP server from scratch!</h1></body></html>"
+
+        case "/about" : 
+			status = "HTTP/1.1 200 Ok\r\n"	
+			body = "<html><body><h1>This is the about page</h1></body></html>"
+
+        default : 
+			status = "HTTP/1.1 404 NOT FOUND\r\n"
+			body = "<html><body><h1>Page could not be found</h1></body></html>"
+		}
+
+		// Set up the HTTP response format
+		responseText :=  status +
 		           		"Content-Type: text/html\r\n" +
 						 fmt.Sprintf("Content-Length: %d\r\n", len(body))+
-						 "Connection: close\r\n" +
+						//  "Connection: close\r\n" +
 						 "\r\n" +
 						  body
-   
+        
+		// Here the server rites to the client
         _, err = conn.Write([]byte(responseText))
 
 		if err!= nil{
